22c585a518fa063867679ffcad9fbe66355e015c
Garik Apreyan <garik.apreyan@gmail.com>
2019-08-24 15:07:24 +0400

    Updates

---
+++ precache-manifest.c7b65ae0ae3a662aa3e75f9393a86b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c67c28f4974b973aaf0218a15c8bb7a",
+    "url": "/Kinoman/index.html"
+  },
+  {
+    "revision": "e6ad630a6ed91be2811a",
+    "url": "/Kinoman/static/css/main.294c932c.chunk.css"
+  },
+  {
+    "revision": "b8c556f4931eb7dcbbf2",
+    "url": "/Kinoman/static/js/2.c4f5d33d.chunk.js"
+  },
+  {
+    "revision": "e6ad630a6ed91be2811a",
+    "url": "/Kinoman/static/js/main.e4b27bc6.chunk.js"
+  },
+  {
+    "revision": "a74f146d50f43a547232",
+    "url": "/Kinoman/static/js/runtime~main.1c4b6da3.js"
+  },
+  {
+    "revision": "4de70e25b3426483f22dad63673b0592",
+    "url": "/Kinoman/static/media/login-back.4de70e25.jpg"
+  }
+]);
